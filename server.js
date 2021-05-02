e39544e8ab73d89e4417b1808d4db51c67b70128
Shim21 <shim21@Shim21s-MacBook-Air.local>
2021-05-02 16:02:38 -0700

    comments

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
+// modules that are needed
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
+
+// database
 const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/database')
+
+// routes
 const authRoutes = require('./routes/auth')
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+// retrieve the .env passwords from file
+require('dotenv').config({path: 'config.env'})
 
-// Passport config
+// retrieve passport features from file
 require('./config/passport')(passport)
 
+// create database
 connectDB()
 
+// set and use modules in app
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// Sessions
+// define session information which includes database for the app
 app.use(
     session({
       secret: 'keyboard cat',
@@ -30,16 +38,17 @@ app.use(
       store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
   )
-  
-// Passport middleware
+
+// use passport middleware to get Google oauth20 into app
 app.use(passport.initialize())
 app.use(passport.session())
 
-  
+// use routes defined above in app
 app.use('/', homeRoutes)
 app.use('/auth', authRoutes)
 app.use('/todos', todoRoutes)
- 
+
+// listen for result, if the app wworks
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
-})    
+})
